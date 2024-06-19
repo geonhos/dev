d1dc9d11c3c7687dd09d647640c988442ea5b72c
geonhos <ghyeom.dev@gmail.com>
2024-06-19 15:13:54 +0900

    OAS를 이용한 swagger-ui 구현 환경 설정
    OpenAPI Specification 3.0 및 swagger ui 를 사용
    기본 환경 설정

---
+++ java/swagger/swagger/src/main/java/com/dev/java/swagger/swagger/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.dev.java.swagger.swagger.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Swagger API")
+                .description("This is a sample Spring Boot RESTful service using Swagger.")
+                .version("1.0");
+    }
+}
